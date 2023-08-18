58f441cf284d070fc64230eac84d4be20b931850
ninegak <113435523+ninegak@users.noreply.github.com>
2023-08-18 21:00:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("home").addEventListener("click", function(event) {
+  event.preventDefault(); 
+  window.location.href = "index.html";
+});
+
+document.getElementById("menu").addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.href = "menu.html";
+});
+
+document.getElementById("shop").addEventListener("click", function(event) {
+  event.preventDefault(); 
+  window.location.href = "menu.html";
+});
+
+document.getElementById("about").addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.href = "about.html";
+});
+
+document.getElementById("cart").addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.href = "menu.html";
+});
